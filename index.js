e02fd6529689e78f17d69f21b56c1c85de6e0403
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-09-10 19:14:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,39 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+// lakshmi_hadi
+// Lucky@2406
+
+// mangodb connection
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://lakshmi_hadi:<db_password>@yoga-master.bywhm.mongodb.net/?retryWrites=true&w=majority&appName=Yoga-master";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
 res.send('Hello Developers 2025!')
 })
