5dd17bcf907b10687e2c81637f8c348eecb11031
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-10-26 19:04:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -164,6 +164,11 @@ async function run() {
     app.post('/create-payment-intent', async (req, res) =>{
       const { price } = req.body;
       const amount = parseInt(price) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      })
     })
 
     // Send a ping to confirm a successful connection
